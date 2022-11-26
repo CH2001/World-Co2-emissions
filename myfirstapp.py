32d87ee88acbe687e4ea8db413d20f6d457d1393
CHUN HANSSEN <65500133+CH2001@users.noreply.github.com>
2022-11-27 01:01:32 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -133,16 +133,4 @@ if option=='ASEAN':
     st.plotly_chart(fig5, use_container_width=True) 
     
 else:
-    'Starting a long computation...'
-
-    
-    latest_iteration = st.empty()
-    bar = st.progress(0)
-
-    for i in range(100):
-   
-        latest_iteration.text(f'Iteration {i+1}')
-        bar.progress(i + 1)
-        time.sleep(0.1)
-
-    '...and now we\'re done!'
+    st.text(" ") 
